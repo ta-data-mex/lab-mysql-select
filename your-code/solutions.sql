46bdc1c1034239938e918bf39cc313ef8f6d6277
eduarde1 <e.castrobermudez@comunidad.unam.mx>
2019-07-09 13:11:46 -0500

    Final

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -9,13 +9,14 @@ ON titles.pub_id = publishers.pub_id;
 
 SELECT authors.au_id AS AUTHOR_ID, authors.au_lname AS LAST_NAME, authors.au_fname AS FIRST_NAME, publishers.pub_name AS PUBLISHER, COUNT(titles.title_id) AS TITLE_COUNT
 FROM publications.authors 
-INNER JOIN publications.titleauthor
+LEFT JOIN publications.titleauthor
 ON authors.au_id = titleauthor.au_id
-INNER JOIN publications.titles
+LEFT JOIN publications.titles
 ON titleauthor.title_id = titles.title_id
 INNER JOIN publications.publishers
 ON titles.pub_id = publishers.pub_id
-GROUP BY authors.au_id;
+GROUP BY authors.au_id, publishers.pub_name
+ORDER BY AUTHOR_ID DESC;
 
 SELECT authors.au_id AS AUTHOR_ID, authors.au_lname AS LAST_NAME, authors.au_fname AS FIRST_NAME, SUM(sales.qty) AS TOTAL
 FROM publications.authors
