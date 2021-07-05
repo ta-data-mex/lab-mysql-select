728377c79de277327920817f01ba10ac64c53b94
Jesus Escamilla <jesusxvix@hotmail.com>
2021-07-05 09:35:49 -0500

    solution

--- your-code/solution.sql
+++ your-code/solution.sql
@@ -32,3 +32,15 @@ INNER JOIN publications.authors ON authors.au_id = titleauthor.au_id
 INNER JOIN publications.sales ON titleauthor.title_id = sales.title_id 
 GROUP BY sales.qty 
 ORDER BY TOTAL desc;
+------------------------------------
+
+SELECT authors.au_id, authors.au_lname, authors.au_fname, (((SUM(sales.qty))*royaltyper/100*price) + SUM(advance*royaltyper/100))  AS PROFIT
+FROM publications.authors
+inner JOIN publications.titleauthor
+ON authors.au_id = titleauthor.au_id 
+inner JOIN publications.titles
+ON titleauthor.title_id = titles.title_id 
+inner JOIN publications.sales
+ON titles.title_id = sales.title_id 
+GROUP BY au_id 
+ORDER BY PROFIT desc LIMIT 3;
